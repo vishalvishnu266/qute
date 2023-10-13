5ea091917026357825a87d8c344cc8ef1e063a31
vishal <vishalvishnu266@gmail.com>
2023-10-13 21:12:37 +0530

    added docker compose file

--- src/test/java/org/acme/qute/HelloResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.acme.qute;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class HelloResourceIT extends HelloResourceTest {
-
-    // Execute the same tests but in native mode.
-}
